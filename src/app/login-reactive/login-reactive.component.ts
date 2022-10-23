930a3510e364a1e87a8e4fedd391214e064cf42a
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-10-23 11:26:56 -0500

    25. The Form Builder API - Writing much more concise Reactive Forms

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { createPasswordStrengthValidator } from "../validators/password-strength.validator";
 
 @Component({
@@ -9,24 +9,22 @@ import { createPasswordStrengthValidator } from "../validators/password-strength
 })
 export class LoginReactiveComponent implements OnInit {
 
-  email = new FormControl("", {
-    validators: [Validators.required, Validators.email],
-    updateOn: "blur"
-  });
-  password = new FormControl("", {
-    validators: [
+  form = this.fb.group({
+    email: ["", {
+      validators: [
+        Validators.required, 
+        Validators.email
+      ],
+      updateOn: "blur"
+    }],
+    password: ["", [
       Validators.required, 
       Validators.minLength(8),
       createPasswordStrengthValidator()
-    ],
-  }) 
-
-  form = new FormGroup({
-    email: this.email,
-    password: this.password
+    ]]
   });
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
 }
