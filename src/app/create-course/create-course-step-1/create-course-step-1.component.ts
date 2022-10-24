41a367dc4f96ed87496f45485577548e4736b839
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-10-24 18:22:32 -0500

    30. Building an Angular Multi-Step Reactive Form with Angular Material

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -11,6 +11,16 @@ import {filter} from 'rxjs/operators';
 })
 export class CreateCourseStep1Component implements OnInit {
 
+  form = this.fb.group({
+    title: ["", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(60)
+    ]]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit() {
 
   }
