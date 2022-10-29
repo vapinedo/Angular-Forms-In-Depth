0ce45f231e0d57b3b5a35d8730307e400387b11b
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-10-29 10:26:22 -0500

    38. Reactive Forms Multi-field Custom Validation

---
+++ src/app/validators/date-range.validator.ts
@@ -0,0 +1,13 @@
+import { ValidatorFn, FormGroup, Validators } from "@angular/forms";
+
+export function createPromoRangeValidator(): ValidatorFn {
+    return (form: FormGroup): Validators | null => {
+        const start: Date = form.get("promoStarAt").value;
+        const end: Date = form.get("promoEndAt").value;
+        if (start && end) {
+            const isRangeValid = (end.getTime() - start.getTime() > 0);
+            return isRangeValid ? null : { promoPeriod: true };
+        }
+        return null;
+    }
+}
