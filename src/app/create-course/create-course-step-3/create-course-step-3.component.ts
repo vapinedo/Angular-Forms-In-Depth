5b7d13dacb0470cb8c64043e72be27b8d63c9ad0
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-11-03 20:34:11 -0500

    53. Angular Form Arrays - Step-by-Step Example

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -10,8 +10,22 @@ import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CreateCourseStep3Component {
 
   form = this.fb.group({
-    
+    lessons: this.fb.array([])
   });
 
   constructor(private fb: FormBuilder) {}
+
+  get lessons() { return this.form.controls["lessons"] as FormArray; }
+
+  addLesson(): void {
+    const lessonForm = this.fb.group({
+      title: ["", Validators.required],
+      level: ["beginner", Validators.required]
+    });
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number): void {
+    this.lessons.removeAt(lessonIndex);
+  }
 }
